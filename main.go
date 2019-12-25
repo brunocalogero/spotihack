9bc0f790c4379df0ede350624bee6a5f6cfb0040
Bruno Calogero <brunocalogero@Brunos-MacBook-Pro.local>
2019-12-25 22:17:22 +0000

    Get basic spotify API working and retrieve hardcoded playlist

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func main() {
+
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotify.TokenURL,
+	}
+	token, err := config.Token(context.Background())
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	client := spotify.Authenticator{}.NewClient(token)
+
+	playlist, err := client.GetPlaylist("3CUKkyF0LGlg0HDSof03Cq")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return
+	}
+
+	fmt.Print(playlist)
+
+}
