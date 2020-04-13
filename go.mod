e096c0724d680c31bb6c2a5cd5aa865ba7d3b642
Bruno Calogero <bruno@riskledger.com>
2020-04-13 22:03:28 +0100

    misc go changes

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/brunocalogero/spotihack
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/zmb3/spotify v0.0.0-20200326174414-8f14a177be3a
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/appengine v1.6.5 // indirect
+)
--- main.go
+++ main.go
@@ -10,6 +10,24 @@ import (
 	"golang.org/x/oauth2/clientcredentials"
 )
 
+// single song retriever
+// playlist retriever
+type playlist struct {
+	Songs []song
+}
+
+type song struct {
+	Artists  []string `json:"artists"`
+	Name     string   `json:"name"`
+	Duration int      `json:"duration"`
+}
+
+func msToSeconds(duration int) int {
+	trackSeconds := (duration / 1000)
+
+	return trackSeconds
+}
+
 func main() {
 
 	config := &clientcredentials.Config{
@@ -24,6 +42,7 @@ func main() {
 
 	client := spotify.Authenticator{}.NewClient(token)
 
+	// NOTE: requires playlist ID
 	playlist, err := client.GetPlaylist("3CUKkyF0LGlg0HDSof03Cq")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
@@ -31,12 +50,25 @@ func main() {
 	}
 
 	for _, track := range playlist.Tracks.Tracks {
-		fmt.Println("")
-		for _, artists := range track.Track.Artists {
-			fmt.Println(artists.Name)
+		var artists []string
+		for _, artist := range track.Track.Artists {
+			artists = append(artists, artist.Name)
 		}
-		fmt.Println(track.Track.Duration)
-		fmt.Println(track.Track.Name)
+
+		// calculate duration from int (ms) to 0:00 minute/seconds
+		trackDurationSeconds := msToSeconds(track.Track.Duration)
+
+		// remove 'original mix' and 'radio edit' from track name?
+
+		// TODO: add date added to playlist + album?
+		songPayload := &song{
+			Artists: artists,
+			Name:    track.Track.Name,
+			// track duration in minutes
+			Duration: trackDurationSeconds,
+		}
+
+		fmt.Println(songPayload)
 	}
 
 }
